8329257f374fcc6d8668fb404da98f554cc0e419
Jinkly <solomone724@gmail.com>
2021-09-16 13:18:40 -0700

    main.py code was reset

--- Exercise/main.py
+++ Exercise/main.py
@@ -1,11 +1,9 @@
 # Individual Github Exercise
-from feature_1 import*
 
-# Prints text asking for number of user input
-num = int(input("Enter number of elements : "))
+# number of elements
+n = int(input("Enter number of elements : "))
 
-# Read inputs from user using map() function
-user_input = list(map(int, input("\nEnter the numbers : ").strip().split()))[:num]
+# Below line read inputs from user using map() function
+a = list(map(int, input("\nEnter the numbers : ").strip().split()))[:n]
 
-sort_int(user_input)
-print("\nList is - ", user_input)
+print("\nList is - ", a)
