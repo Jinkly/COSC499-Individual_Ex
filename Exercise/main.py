a9ccc037454f18d8360e814bf939f44271c1bd19
Jinkly <solomone724@gmail.com>
2021-09-16 13:12:42 -0700

    code prior to first merge

--- Exercise/main.py
+++ Exercise/main.py
@@ -1,16 +1,9 @@
 # Individual Github Exercise
 
-# Function that sorts integers
-def sort_int(user_list):
-    user_list.sort()
-    return user_list
+# number of elements
+n = int(input("Enter number of elements : "))
 
+# Below line read inputs from user using map() function
+a = list(map(int, input("\nEnter the numbers : ").strip().split()))[:n]
 
-# Prints text asking for number of user input
-num = int(input("Enter number of elements : "))
-
-# Read inputs from user using map() function
-user_input = list(map(int, input("\nEnter the numbers : ").strip().split()))[:num]
-
-sort_int(user_input)
-print("\nList is - ", user_input)
+print("\nList is - ", a)
